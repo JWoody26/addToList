15322212714ab586a522137ba5d6d0321779947b
James Woody <james.woody.26@gmail.com>
2022-12-02 13:49:23 -0500

    advance add

--- src/addToList.jsx
+++ src/addToList.jsx
@@ -2,20 +2,65 @@ import { useState, useRef } from 'react'
 import './App.css'
 
  function App(){
-    const [toDoList, setToDoList] = useState([])
-    const inputRef = useRef("");
+    const [toDoList, setList] = useState([])
+    const [currentList, setCurrentList] = useState("")
+    const inputRef = useRef();
+
+  
+    console.log(toDoList)
 
     return(
     <div className = "App">
+        
+        <h1>To Do List</h1>
         <input type="text" ref={inputRef} />
-        <button onClick={() => setToDoList([...toDoList, inputRef.current.value])}>Add to do</button>
+       
+     <button onClick={() => 
+        { 
+            setCurrentList((currentList) => currentList = inputRef.current.value)
+            
+            setList((prevToDoList) => {
+                const thisList = {
+                    listName: inputRef.current.value,
+                    listItems: [],
+                }
+                return [...prevToDoList, thisList]
+          })
+        } 
+        }>Add list</button>
+        
 
-            <ul>{
-            toDoList.map(function (value, index){
-                return <li>{value}</li>
-            })
+        <button onClick={() => 
+            setList(toDoList.map((value, index) => { 
+            if(currentList == value.listName) {
+             value.listItems = [...value.listItems, inputRef.current.value]
             }
+            return value
+        }))
+        }>Add to do</button>
+
+        <div>
+            <h2> Current List: {currentList}</h2>
+        {
+            toDoList.map( (value, index) =>{
+                return <ul> 
+                    
+                    <button onClick= {() => {
+                    setCurrentList(function (currentList) {
+                        return currentList = value.listName
+                    })   
+                 }}> {value.listName} </button>
+
+                 {value.listItems.map(function (value, index) {
+                    return <li>{value}</li>
+                 })
+                 }
             </ul>
+            })
+        }
+ 
+        </div>
+      
     </div>
     )
 }
